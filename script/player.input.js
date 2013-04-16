998748b3e98aeb882fb1d7f5b8898f577b3895b2
Retro Gamer <andre_newman2@hotmail.com>
2013-04-16 07:22:28 -0400

    removing vhs

--- script/player.input.js
+++ script/player.input.js
@@ -47,12 +47,6 @@ Player.prototype.clearInput = function(record)
         this.ButtonState[i].Frame = 0;
     }
     this.ButtonStates = [];
-
-    if(!!record)
-    {
-        if(game_.isRecording())
-            game_.recordInput(this.Team,this.Index,this.Folder,null,null,this.getFrame(),"clearInput");
-    }
 }
 
 //Ensures that the buttonStateChange array doesn't get too big
@@ -162,16 +156,12 @@ Player.prototype.onKeyStateChanged = function(isDown,keyCode,frame)
             //the button was just pressed
             this.ButtonState[key].Value = BUTTON_STATE.PRESSED
             this.ButtonState[key].Frame = frame;
-            if(game_.isRecording())
-                game_.recordInput(this.Team,this.Index,this.Folder,isDown,key,this.getInputFrame(frame));
         }
         else if(!isDown && (this.ButtonState[key].Value == BUTTON_STATE.PRESSED))
         {
             //the button was released
             this.ButtonState[key].Value = BUTTON_STATE.NONE;
             this.ButtonState[key].Frame = frame;
-            if(game_.isRecording())
-                game_.recordInput(this.Team,this.Index,this.Folder,isDown,key,this.getInputFrame(frame));
         }
         else
             return;
